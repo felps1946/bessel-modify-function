4f2eae30137e7fe1f7821faddf9c3883c1aa91cf
felps1946 <felipesilva.s@ufba.br>
2024-08-17 00:27:13 -0300

    Update type2.py

--- type2.py
+++ type2.py
@@ -1,31 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import gamma
 
-def K_v(v, x, n_terms=100):
-    if x == 0:
-        return np.inf 
-    sum_kv = 0
-    for n in range(n_terms):
-        term = ((-1)**n) * (x/2)**(2*n + v) / (np.prod(np.arange(1, n + 1)) * np.prod(np.arange(1, n + v + 1)))
-        sum_kv += term
-    return (np.pi / (2 * np.sin(np.pi * v))) * sum_kv
+def K_v(v, x):
+    if np.sin(v * np.pi) == 0:
+        raise ValueError("K_v is undefined for integer values of v due to division by zero.")
+    
+    sum_term1 = 0
+    sum_term2 = 0
+    k = 0
+    
+    term1 = (x / 2)**(2 * k - v) / (gamma(k + 1) * gamma(k - v + 1))
+    while k < 100:  
+        if k >= v: 
+            sum_term1 += term1
+        k += 1
+        term1 = (x / 2)**(2 * k - v) / (gamma(k + 1) * gamma(k - v + 1))
+    
+    k = 0  
 
-x = np.linspace(0.01, 10, 400)
+    term2 = (x / 2)**(2 * k + v) / (gamma(k + 1) * gamma(k + v + 1))
+    while k < 100: 
+        sum_term2 += term2
+        k += 1
+        term2 = (x / 2)**(2 * k + v) / (gamma(k + 1) * gamma(k + v + 1))
+    
+    return (np.pi / (2 * np.sin(v * np.pi))) * (sum_term1 - sum_term2)
 
-v_values = [0, 1, 2] 
+
+x = np.linspace(0.1, 10, 400)  
+
+v_values = [0.5, 1.5, 2.5]
+
 
 plt.figure(figsize=(12, 6))
 
-plt.subplot(1, 2, 1)  
+
+plt.subplot(1, 2, 1)
 for v in v_values:
     plt.plot(x, [K_v(v, xi) for xi in x], label=f'K_{v}(x)')
-plt.title('Funções Modificadas de Bessel do 2º Tipo')
+plt.title('Funções Modificadas de Bessel do Segundo Tipo')
 plt.xlabel('x')
-plt.xlim(0, 6)
-plt.ylabel('K_n(x)')
+plt.xlim(0, 10) 
+plt.ylabel('K_v(x)')
 plt.ylim(0, 10)  
 plt.legend()
 plt.grid()
+
 
 plt.tight_layout()
 plt.show()
