c65b6455ea525c47eff1dbf4b6bf189cd18b1bac
felps1946 <felipesilva.s@ufba.br>
2024-08-17 00:20:48 -0300

    Update type2.py

--- type2.py
+++ type2.py
@@ -1,19 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import kv
 
-x = np.linspace(0.01, 10, 100)  # Evitar x=0 para não ter singularidade
-v_values = [0, 1, 2]
+def K_v(v, x, n_terms=100):
+    if x == 0:
+        return np.inf  # K_v(0) é infinito para v >= 0
+    sum_kv = 0
+    for n in range(n_terms):
+        term = ((-1)**n) * (x/2)**(2*n + v) / (np.prod(np.arange(1, n + 1)) * np.prod(np.arange(1, n + v + 1)))
+        sum_kv += term
+    return (np.pi / (2 * np.sin(np.pi * v))) * sum_kv
 
-plt.figure(figsize=(10, 6))
+x = np.linspace(0.01, 10, 400)
+
+v_values = [0, 1, 2] 
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)  
 for v in v_values:
-    plt.plot(x, kv(v, x), label=f'K_{v}(x)')
-
+    plt.plot(x, [K_v(v, xi) for xi in x], label=f'K_{v}(x)')
 plt.title('Funções Modificadas de Bessel do 2º Tipo')
 plt.xlabel('x')
-plt.xlim(0, 4)  
-plt.ylabel('K_v(x)')
-plt.ylim(0, 3.5)  # Ajustar o limite do eixo y para melhor visualização
+plt.xlim(0, 6)
+plt.ylabel('K_n(x)')
+plt.ylim(0, 10)  
 plt.legend()
 plt.grid()
+
+plt.tight_layout()
 plt.show()
